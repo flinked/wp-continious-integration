ffafcc1cd5691e0a60acffba78b5dabbf5ebb484
Alexis Benoliel <alexbenoliel@gmail.com>
2019-01-27 13:32:51 +0100

    ✨ add slack notification

---
+++ notify/complete.js
@@ -0,0 +1,12 @@
+// Require module:
+var MY_SLACK_WEBHOOK_URL = 'yourapiurl';
+var slack = require('slack-notify')(MY_SLACK_WEBHOOK_URL);
+
+
+slack.alert({
+  text: ':sparkles: New version alive',
+  fields: {
+    'Information': 'front & Data base updated',
+    'link': 'www.sitename.flinked.fr'
+  },
+});
---
+++ notify/front.js
@@ -0,0 +1,12 @@
+// Require module:
+var MY_SLACK_WEBHOOK_URL = 'yourapiurl';
+var slack = require('slack-notify')(MY_SLACK_WEBHOOK_URL);
+
+
+slack.alert({
+  text: ':sparkles: New version alive',
+  fields: {
+    'Information': 'front updated',
+    'link': 'www.sitename.flinked.fr'
+  },
+});
